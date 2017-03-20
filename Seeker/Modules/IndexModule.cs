b5ec21e8189189a79335c506e45e4a0e7c0b4f8e
vlasovsv <vlasovsv@gmail.com>
2017-03-20 23:37:59 +0300

    (+) Added a start page for searching. (+) Added Nancy module that will handle queries from search page. (*) Configure NLog in order to write all log information to console.

---
+++ Seeker/Modules/IndexModule.cs
@@ -0,0 +1,25 @@
+﻿using Nancy;
+
+namespace Seeker.Modules
+{
+    /// <summary>
+    /// Represents a controller of a start searching page.
+    /// </summary>
+    public class IndexModule : NancyModule
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Creates an index module.
+        /// </summary>
+        public IndexModule()
+        {
+            Get("/", _ =>
+            {
+                return Negotiate.WithView("index.html");
+            });
+        }
+
+        #endregion
+    }
+}
