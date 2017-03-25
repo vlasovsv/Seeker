384707ce90b6780415a8bcc1b8b9b74348e60a75
birdy90 <go.birdy.90@gmail.com>
2017-03-25 22:16:43 +0300

    (*) first attepts to search

---
+++ Seeker/Content/seeker.js
@@ -0,0 +1,26 @@
+﻿$(function () {
+    $('.search-container input').on('keypress', function (e) {
+        if (e.which === 13)
+            doSearch();
+    });
+
+    $('.search-container .js-search-button').on('click', doSearch);
+});
+
+function doSearch() {
+    var tbody = $('.results-container .js-log-table tbody');
+    $('.results-container .js-log-table-container').addClass('hidden');
+    tbody.html('');
+    $('.results-container .loader').removeClass('hidden');
+    $.get('/search', { 'q': $('.search-container input').val() }, function (data) {
+        $('.results-container .loader').addClass('hidden');
+        $('.results-container .js-log-table-container').removeClass('hidden');
+        data.forEach(function (e) {
+            let template = $('.js-table-row-template .table-row').clone();
+            template.find('.time').html(e.timestamp);
+            template.find('.message').html(e.message);
+            template.find('.machine').html(e.properties.machine);
+            tbody.append(template);
+        });
+    }, 'json');
+}
