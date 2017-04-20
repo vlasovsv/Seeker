7fa76f139794060c733c56b929e3faaf3184621a
vlasovsv <vlasovsv@gmail.com>
2017-04-21 00:28:22 +0300

    (+) Added local cache of js and css components in order to work without access to the Internet. (+) Added datetime pickers to choose a date range.

--- Seeker/Content/seeker.js
+++ Seeker/Content/seeker.js
@@ -5,6 +5,17 @@
     });
 
     $('.search-container .js-search-button').on('click', doSearch);
+
+    $('#startdatepicker').datetimepicker();
+    $('#enddatepicker').datetimepicker({
+        useCurrent: false //Important! See issue #1075
+    });
+    $("#startdatepicker").on("dp.change", function (e) {
+        $('#enddatepicker').data("DateTimePicker").minDate(e.date);
+    });
+    $("#enddatepicker").on("dp.change", function (e) {
+        $('#startdatepicker').data("DateTimePicker").maxDate(e.date);
+    });
 });
 
 function doSearch() {
@@ -12,13 +23,19 @@ function doSearch() {
     $('.results-container .js-log-table-container').addClass('hidden');
     tbody.html('');
     $('.results-container .loader').removeClass('hidden');
-    $.get('api/v1/search', { 'q': $('.search-container input').val() }, function (data) {
-        $('.results-container .loader').addClass('hidden');
-        $('.results-container .js-log-table-container').removeClass('hidden');
-        let template = $.templates('#log-row-template');
-        data.forEach(function (e) {
-            tbody.append($(template.render(e)));
-        });
+    $.get('api/v1/search',
+        {
+            'q': $('.search-container input').val(),
+            'start': $('#startdatepicker').data("DateTimePicker").date().format('YYYY-MM-DDTHH:mm:ss'),
+            'end': $('#enddatepicker').data("DateTimePicker").date().format('YYYY-MM-DDTHH:mm:ss')
+        },
+        function (data) {
+            $('.results-container .loader').addClass('hidden');
+            $('.results-container .js-log-table-container').removeClass('hidden');
+            let template = $.templates('#log-row-template');
+            data.forEach(function (e) {
+                tbody.append($(template.render(e)));
+            });
     }, 'json');
 }
 
--- Seeker/Modules/SearchModule.cs
+++ Seeker/Modules/SearchModule.cs
@@ -4,6 +4,7 @@ using Autofac;
 using Nancy;
 
 using Seeker.Searching;
+using System;
 
 namespace Seeker.Modules
 {
@@ -30,7 +31,9 @@ namespace Seeker.Modules
 
             Get("/search", parameters =>
             {
-                var request = new SearchRequest(this.Request.Query["q"]);
+                var start = DateTime.Parse(this.Request.Query["start"]);
+                var end = DateTime.Parse(this.Request.Query["end"]);
+                var request = new SearchRequest(this.Request.Query["q"], start, end);
                 var results = _lucene.Search(request);
                 return Response.AsJson(results.OrderByDescending(x => x.Timestamp));
             });
