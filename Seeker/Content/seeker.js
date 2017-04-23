b3c7c2503fc9e6c673b18f00e619a15e8e08c257
vlasovsv <vlasovsv@gmail.com>
2017-04-23 11:14:56 +0300

    (*) Added an ability to expand a list of properties, and exception info.

--- Seeker/Content/seeker.js
+++ Seeker/Content/seeker.js
@@ -6,8 +6,11 @@
 
     $('.search-container .js-search-button').on('click', doSearch);
 
-    $('#startdatepicker').datetimepicker();
+    $('#startdatepicker').datetimepicker({
+        defaultDate: moment().add(-7, 'days')
+    });
     $('#enddatepicker').datetimepicker({
+        defaultDate: moment(),
         useCurrent: false //Important! See issue #1075
     });
     $("#startdatepicker").on("dp.change", function (e) {
@@ -34,7 +37,7 @@ function doSearch() {
             $('.results-container .js-log-table-container').removeClass('hidden');
             let template = $.templates('#log-row-template');
             data.forEach(function (e) {
-                tbody.append($(template.render(e)));
+                tbody.append($(template.render(e, { link: false })));
             });
     }, 'json');
 }
@@ -43,5 +46,23 @@ $.views.helpers({
     dateFormat: function (val, format) {
         var date = moment(val);
         return date.format(format);
+    },
+    levelLabel: function (val) {
+        var label = 'label-primary';
+        switch (val)
+        {
+            case 'Warn':
+                label = 'label-warning';
+                break;
+            case 'Error':
+            case 'Fatal':
+                label = 'label-danger';
+                break;
+        }
+        return 'label ' + label;
     }
 });
+
+$(document).on('click', '.expander', function () {
+    $(this).siblings('div').toggleClass('hidden');
+})
